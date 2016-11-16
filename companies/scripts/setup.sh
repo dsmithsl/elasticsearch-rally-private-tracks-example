219bbda56cbcf7c738007402158618147ab59446
Darren Peter Smith <darren.smith@mergermarket.com>
2016-11-16 16:43:22 +0000

    Initial commit

---
+++ companies/scripts/setup.sh
@@ -0,0 +1,17 @@
+#! /bin/bash
+DATA_PATH=~/.rally/benchmarks/data/companies
+FILENAME=documents
+DOWNLOAD_URL=https://opendata.camden.gov.uk/api/views/iix4-id37/rows.csv?accessType=DOWNLOAD
+
+if [ ! -d $DATA_PATH ]; then
+    mkdir $DATA_PATH
+fi
+
+#curl -o $DATA_PATH/$FILENAME.csv $DOWNLOAD_URL
+python3 toJsonWithID.py > $DATA_PATH/$FILENAME.json
+bzip2 -9 -c $DATA_PATH/$FILENAME.json > $DATA_PATH/$FILENAME.json.bz2
+
+wc -l $DATA_PATH/$FILENAME.csv | awk {'print "Document count: " $1'}
+stat -f"Uncompressed Bytes: %z" $DATA_PATH/$FILENAME.json
+stat -f"Compressed Bytes: %z" $DATA_PATH/$FILENAME.json.bz2
+
